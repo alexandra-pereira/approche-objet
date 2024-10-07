51034b394da958b96ee073f5618f2fdaef04273f
alexandra-pereira <xandrapereira@hotmail.com>
2024-10-07 15:14:01 +0200

    tp 07 HERITAGE ET CONSTRUCTEURS

--- src/banque/TestBanque.java
+++ src/banque/TestBanque.java
@@ -5,8 +5,17 @@ import banque.entites.Compte;
 public class TestBanque {
     public static void main(String[] args) {
 
-        Compte compte = new Compte("FR786487235472354", 2300.45 );
-        System.out.println(compte);
+//        Compte compte = new Compte("FR786487235472354", 2300.45 );
+//        System.out.println(compte);
 
+        Compte[] comptes = new Compte[2];
+
+        comptes[0] = new Compte("FR786487235472354", 2300.45);
+
+        comptes[1] = new Compte("FR2834678452785", 456798.03);
+
+        for (int i = 0; i < comptes.length; i++) {
+            System.out.println(comptes[i]);
+        }
     }
 }
---
+++ src/banque/entites/CompteTaux.java
@@ -0,0 +1,16 @@
+package banque.entites;
+
+public class CompteTaux extends Compte {
+
+    private double tauxRemuneration;
+
+    public CompteTaux(String numeroCompte, double solde, double tauxRemuneration) {
+        super(numeroCompte, solde);
+        this.tauxRemuneration = tauxRemuneration;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", tauxRémunération=" + tauxRemuneration;
+    }
+}
