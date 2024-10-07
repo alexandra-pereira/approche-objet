84a7af4fec6ab71f55687f33304e879f36127f18
alexandra-pereira <xandrapereira@hotmail.com>
2024-10-07 12:09:38 +0200

    tp 05 METTRE EN PLACE LES REGLES DE L’ENCAPSULATION

---
+++ src/banque/TestBanque.java
@@ -0,0 +1,13 @@
+package banque;
+
+import banque.entites.Compte;
+
+public class TestBanque {
+    public static void main(String[] args) {
+
+        Compte compte = new Compte("FR786487235472354", 2300.45 );
+        System.out.println(compte);
+
+
+    }
+}
---
+++ src/banque/entites/Compte.java
@@ -0,0 +1,27 @@
+package banque.entites;
+
+public class Compte {
+    private String numeroCompte;
+    private double solde;
+
+    public Compte(String numeroCompte, double solde) {
+        this.numeroCompte = numeroCompte;
+        this.solde = solde;
+    }
+
+//    public String getNumeroCompte() {
+//        return numeroCompte;
+//    }
+//
+//    public void setNumeroCompte(String numeroCompte) {
+//        this.numeroCompte = numeroCompte;
+//    }
+//
+//    public double getSolde() {
+//        return solde;
+//    }
+//
+//    public void setSolde(double solde) {
+//        this.solde = solde;
+//    }
+}
