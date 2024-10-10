833440340bb16d89a564848b71d79a69393f3122
alexandra-pereira <xandrapereira@hotmail.com>
2024-10-10 15:03:13 +0200

    TP 15 – APPRENDRE A MANIPULER DES FICHIERS - Exercice LireFichierAvecInstanciation

--- src/fichier/LectureFichier.java
+++ src/fichier/LectureFichier.java
@@ -5,16 +5,37 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LectureFichier {
     public static void main(String[] args) throws IOException {
 
+        List<Ville> villeList = new ArrayList<>();
+
         Path pathFile = Paths.get("/Users/alexandrapereira/Documents/CDA-JAVA/JAVA-projects/recensement.csv");
         List<String> lines = Files.readAllLines(pathFile, StandardCharsets.UTF_8);
 
+//        for (String line : lines) {
+//            System.out.println(line);
+//        }
+
+        if (lines.size() > 0) {
+            lines.remove(0); // This will remove the first line (header)
+        }
+
         for (String line : lines) {
             System.out.println(line);
+            String[] tokens = line.split(";");
+            if (tokens.length >= 4) {
+                String nom = tokens[1].trim();
+                String codeDepartement = tokens[2].trim();
+                String nomRegion = tokens[3].trim();
+                int populationTotale = Integer.parseInt(tokens[9].replaceAll(" ", ""));
+
+                Ville ville = new Ville(nom, codeDepartement, nomRegion, populationTotale);
+                villeList.add(ville);
+            }
         }
     }
 }
---
+++ src/fichier/Ville.java
@@ -0,0 +1,16 @@
+package fichier;
+
+public class Ville {
+
+    private String nom;
+    private String codeDepartement;
+    private String nomRegion;
+    private int populationTotale;
+
+    public Ville(String nom, String codeDepartement, String nomRegion, int populationTotale) {
+        this.nom = nom;
+        this.codeDepartement = codeDepartement;
+        this.nomRegion = nomRegion;
+        this.populationTotale = populationTotale;
+    }
+}
